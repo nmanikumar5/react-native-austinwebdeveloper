9c7faebacf6cc034114b3599167173279c8f1005
ManiKumar <nmanikumar5@gmail.com>
2018-07-16 17:34:17 +0530

    Initial Push

--- android/src/main/java/com/nueve/austinwebdeveloper/Module.java
+++ android/src/main/java/com/nueve/austinwebdeveloper/Module.java
@@ -1,4 +1,4 @@
-package io.cmichel.boilerplate;
+package com.nueve.austinwebdeveloper;
 
 import android.widget.Toast;
 
--- android/src/main/java/com/nueve/austinwebdeveloper/Package.java
+++ android/src/main/java/com/nueve/austinwebdeveloper/Package.java
@@ -1,4 +1,4 @@
-package io.cmichel.boilerplate;
+package com.nueve.austinwebdeveloper;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
